d54eb4e64d11b844ab6856bbffd9ab0466d6da68
unknown <aquibjavedmomin.sg@RRSGPC55.readingroom.local>
2015-05-26 16:06:36 +0800

    Navigation Menu Done

--- _templates/includes/primary-nav.php
+++ _templates/includes/primary-nav.php
@@ -1,27 +1,30 @@
-<div id="primary-nav">
+<div id="primary-nav" class="nav-container">
     <nav class="nav" role="navigation">
-        <div class="wrap">
-            <h1>
-                <a href="/">Master Builder</a>
-            </h1>
+        <div class="wrap logo">
+            <a href="/"><img src="/assets/bawarchi/images/main-logo.png" /></a>
         </div>
 
         <ul class="lvl1">
             <li>
-                <a href="#" class="no-link <?php if ($primary == 0){ echo 'active'; }?>"><span>Menu</span></a>
-                <ul class="lvl2">
-                    <li><a href="/standard/"><span>Standard Content</span></a></li>
-                    <li><a href="/grid/"><span>Grid System</span></a></li>
-                    <li>
-                        <a href="#"><span>Level 2</span></a>
-                        <ul class="lvl3">
-                            <li><a href="#"><span>Level 3</span></a></li>
-                            <li><a href="#"><span>Level 3</span></a></li>
-                            <li><a href="#"><span>Level 3</span></a></li>
-                            <li><a href="#"><span>Level 3</span></a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <a href="#" class="home"><span class="icon icon-home3"></span></a>
+            </li>
+            <li>
+                <a href="#" ><span>Resort</span></a>
+            </li>
+            <li>
+                <a href="#" ><span>Recreation</span></a>
+            </li>
+            <li>
+                <a href="#" ><span>Events</span></a>
+            </li>
+            <li>
+                <a href="#" ><span>News</span></a>
+            </li>
+            <li>
+                <a href="#" ><span>Features</span></a>
+            </li>
+            <li>
+                <a href="#" ><span>Contact</span></a>
             </li>
         </ul>
     </nav>
