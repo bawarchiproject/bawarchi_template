a6ec18f86dcd2b3447d93638ddd634f3f973ea1d
unknown <aquibjavedmomin.sg@RRSGPC55.readingroom.local>
2015-05-27 16:03:25 +0800

    Footer Done

--- _templates/includes/footer.php
+++ _templates/includes/footer.php
@@ -1,3 +1,49 @@
+    <footer>
+        <div class="container-fluid">
+            <section class="upper-block">
+                <section class="f-block contact">
+                    <a href="#"><h3><span class="icon icon-location"></span>Contact Us</h3></a>
+                    <p>Line 1</p>
+                    <p>Line 2</p>
+                    <p><span class="icon icon-phone"></span>+91 0000000000</p>
+                    <p><span class="icon icon-mobile"></span>+91 0000000000</p>
+                </section>
+
+                <section class="f-block">
+                    <a href="#"><h3>About Us</h3></a>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesettin</p>
+                </section>
+
+                <section class="f-block">
+                    <a href="#"><h3>Advertise</h3></a>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesettin</p>
+                </section>
+
+                <section class="f-block">
+                    <a href="#"><h3>Why Us?</h3></a>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesettin</p>
+                </section>
+
+                <section class="f-block">
+                    <a href="#"><h3>Blog</h3></a>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesettin</p>
+                </section>
+            </section>  
+
+            <section class="lower-block">
+                <div class="coypright pull-left">
+                    <p>&copy; 2015 Powered by Your Company Name</p>
+                </div>
+
+                <ul class="social-media pull-right">
+                    <li><a href="#" ><span class="icon icon-feed"></span><span class="visuallyhidden">RSS</span></a></li>
+                    <li><a href="#" ><span class="icon icon-facebook"></span><span class="visuallyhidden">Facebook</span></a></li>
+                    <li><a href="#" ><span class="icon icon-twitter"></span><span class="visuallyhidden">Twitter</span></a></li>
+                    <li><a href="#" ><span class="icon icon-linkedin2"></span><span class="visuallyhidden">Linkedin</span></a></li>
+                </ul>
+            </section>  
+        </div>
+    </footer>
         <!-- build:js //ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js -->
         <script src="/assets/bawarchi/js/vendor/jquery-1.11.3.min.js"></script>
         <!-- /build -->
