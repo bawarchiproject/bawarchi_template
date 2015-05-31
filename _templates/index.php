ce0ef930f7ca53f0daf76e6463c762c80c428d40
Mayur Lunawat <lunawatmayur8@gmail.com>
2015-05-31 15:09:13 +0530

    Main page - best cooks section done. Added placeholder instead of value in text field for search section.

--- _templates/index.php
+++ _templates/index.php
@@ -16,13 +16,13 @@
 							<img src="assets/bawarchi/images/map-arrow-icon.png">
 						</div>
 						<div class="select-location-text">
-							<input type="text" class="textBox" value="Select the location">
+							<input type="text" class="textBox" placeholder="Select the location">
 						</div>
 					</div>
 					<div class="inner-section-space"></div>
 					<div class="inner-section-select-city">
 						<div class="select-city-text">
-							<input type="text" class="textBox" value="Select the city">
+							<input type="text" class="textBox" placeholder="Select the city">
 						</div>
 					</div>
 					<div class="inner-section-search-button">Search</div>
@@ -30,11 +30,25 @@
 			</div>		
 		</article>
 	</section>
+</div>
+
 	<section class="container-fluid">
 		<article class="col-12">
+			<div class="main-page-best-cooks">
+  				<div class="main-page-best-cooks-inner-header">
+  					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  				</div>
+  				<div class="main-page-best-cooks-inner-content">
+  					AWARD WINNING COOK(s) IN FUNCTION(s)
+				</div>	
+				<div class="main-page-best-cooks-selection">
+					<button class="main-page-best-cooks-button">
+						What is happening at the test center?
+					</button>
+				</div>
+  			</div>
 		</article>
 	</section>
-</div>
 </main>
 
 
