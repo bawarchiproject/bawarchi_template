a2ed5b1f3dd89a1302f1382260e7b1e177181472
Aquib Momin <aquibmomin007@gmail.com>
2015-06-03 17:19:47 +0530

    Homepage Main body done

--- _templates/index.php
+++ _templates/index.php
@@ -24,6 +24,51 @@
 			</div>
 		</article>
 	</section>
+	<section class="container-fluid tab-container">
+		<ul class="categories">
+			<li class="cat-tab">
+				<img src="/assets/bawarchi/images/tab-1.png" alt="tab-1" />
+				<div class="content">
+					<h3>Caterers</h3>
+					<span class="cat-count">313</span>
+					<h5>Party orders</h5>	
+				</div>
+				<a href="#" class="btn-all">View All</a>
+			</li>
+			<li class="cat-tab">
+				<img src="/assets/bawarchi/images/tab-2.png" alt="tab-2" />
+				<div class="content">
+					<h3>Chefs</h3>
+					<span class="cat-count">234</span>
+					<h5>Professionals</h5>	
+				</div>
+				<a href="#" class="btn-all">View All</a>
+			</li>
+			<li class="cat-tab">
+				<img src="/assets/bawarchi/images/tab-3.png" alt="tab-3" />
+				<div class="content">
+					<h3>Specifics</h3>
+					<span class="cat-count">513</span>
+					<h5>Cuisine caterers</h5>	
+				</div>
+				<a href="#" class="btn-all">View All</a>
+			</li>
+			<li class="cat-tab">
+				<img src="/assets/bawarchi/images/tab-4.png" alt="tab-4" />
+				<div class="content">
+					<h3>Normal</h3>
+					<span class="cat-count">142</span>
+					<h5>Day-today-cook</h5>	
+				</div>
+				<a href="#" class="btn-all">View All</a>
+			</li>
+		</ul>
+
+		<article class="custom-block">
+			<h3>AWARD WINNING <span class="ul">COOK(s)</span> IN FUNCTION(s)</h3>
+			<a href="#" class="btn-large">What is happening at the Test Center?</a>
+		</article>
+	</section>
 </div>
 
 </main>
